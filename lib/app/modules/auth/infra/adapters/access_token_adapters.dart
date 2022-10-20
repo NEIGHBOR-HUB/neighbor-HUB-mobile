74db0ea79a04c6c875d6647da3cfaec8e9119b95
Nityananda Barbosa <nityanandabarbosa@gmail.com>
2022-10-20 20:54:16 -0300

    [NHM-004] implements adapters

---
+++ lib/app/modules/auth/infra/adapters/access_token_adapters.dart
@@ -0,0 +1,11 @@
+
+import 'package:neighbor_hub/app/modules/auth/domain/entities/access_token.dart';
+
+class AccessTokenAdapter {
+  static AccessToken fromMap(Map<String, dynamic> map) {
+    return AccessToken(
+      accessToken: map["access_token"],
+      expiresAt: DateTime.parse(map["expires_at"]),
+    );
+  }
+}
---
+++ lib/app/modules/auth/infra/adapters/sing_in_adpters.dart
@@ -0,0 +1,16 @@
+
+import 'package:neighbor_hub/app/modules/auth/domain/entities/sing_in_user.dart';
+
+class SingInParamsAdpters {
+  static SingInParams fromMap(Map<String, dynamic> map) {
+    return SingInParams(
+      email: map['email'] ?? '',
+      password: map['password'] ?? '',
+    );
+  }
+
+  static Map<String, dynamic> toMap(SingInParams params) => <String, dynamic>{
+        "email": params.email,
+        "password": params.password,
+      };
+}
