8a878e383d2e9bee50989d1e17475ef452db28df
Nityananda Barbosa <nityanandabarbosa@gmail.com>
2022-10-20 21:01:30 -0300

    [NHM-004] implements repository with their tests

---
+++ lib/app/modules/auth/infra/repositories/sing_up_repository_impl.dart
@@ -0,0 +1,35 @@
+import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
+import 'package:neighbor_hub/app/modules/auth/domain/entities/sing_up_user.dart';
+import 'package:neighbor_hub/app/modules/auth/domain/errors/sing_up_errors.dart';
+import 'package:neighbor_hub/app/modules/auth/domain/repositories/sing_up_repository.dart';
+import 'package:neighbor_hub/app/modules/auth/infra/datasources/sing_up_datasource.dart';
+import 'package:neighbor_hub/app/modules/auth/infra/errors/sing_up_infra_errors.dart';
+
+
+class SingUpRepositoryImpl implements SingUpRepository {
+  final SingUpDataSource dataSource;
+
+  SingUpRepositoryImpl(this.dataSource);
+
+  @override
+  Future<Either<SingUpException, SingedUser>> userSingUp(
+      SingUpParams params) async {
+    try {
+      final result = await dataSource.userSingUp(params);
+      return Right(result);
+    } on DioError catch (e) {
+      if(e.response?.statusCode == 406){
+        return Left(EmailAlreadyUsed());
+      }
+      if(e.response?.statusCode == 500){
+        return Left(DataSourceError());
+      }
+      return Left(DataSourceError());
+    } on DataSourceError catch (e) {
+      return Left(e);
+    } on Exception {
+      return Left(DataSourceError("Unknow exception"));
+    }
+  }
+}
